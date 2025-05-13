d0eedaa36ebbfaa521f950fe61a5d979f1259593
Antonio Carlos da Silva Barros <antoniocarlos180707@gmail.com>
2025-05-13 15:29:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,8 @@
     const telaInicial = document.getElementById('telaInicial');
     const telaNome = document.getElementById('telaNome');
 
-    let pressTimer;
-
-    btnIniciar.addEventListener('mousedown', () => {
-      pressTimer = setTimeout(() => {
-        telaInicial.style.transform = 'translateY(-100%)';
-        telaNome.style.transform = 'translateY(0%)';
-      }, 2000); // 2 segundos segurando
-    });
-
-    btnIniciar.addEventListener('mouseup', () => {
-      clearTimeout(pressTimer);
-    });
-
-    btnIniciar.addEventListener('mouseleave', () => {
-      clearTimeout(pressTimer);
+    // Simples clique no botão
+    btnIniciar.addEventListener('click', () => {
+      telaInicial.style.transform = 'translateY(-100%)';
+      telaNome.style.transform = 'translateY(0%)';
     });
